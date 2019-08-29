55198d63d4a3da55ffb77a052c51a25f4756f44d
diksha <diksha@bitterntec.com>
2019-08-29 12:23:25 +0530

    download ical file

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,19 +1,5 @@
 class MeetingsController < ApplicationController
 	def show
-    # cal = Icalendar::Calendar.new
-
-    # Event.all.each do |event|
-    #   if event.start_date.present? && event.end_date.present?
-    #   	ical_event = Icalendar::Event.new
-    #     ical_event.dtstart = Icalendar::Values::Date.new(event.start_date.strftime('%Y%m%d'))
-    #     ical_event.dtend   = Icalendar::Values::Date.new(event.end_date.strftime('%Y%m%d'))
-    #     ical_event.summary = event.title
-    #     cal.add_event(ical_event)
-    #   end
-    # end
-    
-    # puts "Event= #{cal.to_ical}"
-    
     respond_to do |format|
       format.ics do
         cal = Icalendar::Calendar.new
@@ -27,26 +13,10 @@ class MeetingsController < ApplicationController
           end 
         end         
         cal.publish 
+        cal_string = cal.to_ical
+        send_data cal_string, :type => 'text/xml; charset=UTF-8;', :disposition => "attachment; filename=ical.ics"
       end
-      format.html  
+      format.html
     end
   end
-
-	# def to_icalendar
-	# 	byebug
-	# 	@event = MeetingEvent.all
-	# 	respond_to do |format|
-	# 		format.ics do
-	# 			cal = Icalendar::Calendar.new
- #          event = Icalendar::Event.new
- #            # event.dtstart = @event.start_date
- #            # event.dtend = @event.end_date 
- #            event.summary = @event.title
- #            event.uid = event.url = "#{event_url}"
- #            cal.add_event(event)            
- #            cal.publish
-            
-	# 		end 
-	# 	end
-	
 end
